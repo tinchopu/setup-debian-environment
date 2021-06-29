d3feaaa723596785d1c9bdfcbcabc4f71c9fa053
tincho <anton@anton-puetz.com>
2021-06-29 09:53:06 +0000

    Removed dotfiles since they are in other repo

--- install_debian.sh
+++ install_debian.sh
@@ -6,10 +6,8 @@ sudo apt remove vim -y
 sudo apt autoremove -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-wget https://gitlab.anton-puetz.com/tincho/setup-debian-environment/-/raw/master/.p10k.zsh -O .p10k.zsh
 mkdir .ssh
 wget https://gitlab.anton-puetz.com/tincho/setup-debian-environment/-/raw/master/ssh_public_keys -O .ssh/authorized_keys
-wget https://gitlab.anton-puetz.com/tincho/setup-debian-environment/-/raw/master/.zshrc -O .zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl https://getcroc.schollz.com | bash
